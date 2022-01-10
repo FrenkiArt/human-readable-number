6fe3daf74d70d5e6a5a4644b330460cd84d88f0b
frenkiart <frenki_art@mail.ru>
2022-01-11 01:25:06 +0300

    feat: almost done

--- src/index.js
+++ src/index.js
@@ -35,13 +35,25 @@ module.exports = function toReadable(number) {
         80: "eighty",
         90: "ninety",
     };
-    const objectHundredths = [];
+
+    const objectHundreds = {
+        100: "one hundred",
+        200: "two hundred",
+        300: "three hundred",
+        400: "four hundred",
+        500: "five hundred",
+        600: "six hundred",
+        700: "seven hundred",
+        800: "eight hundred",
+        900: "nine hundred",
+    };
 
     if (strLenght === 1) {
         result = returnSingleNumber(strNumber);
     } else if (strLenght === 2) {
         result = returnDecimalNumber(strNumber);
     } else if (strLenght === 3) {
+        result = returnHundredthsNumber(strNumber);
     } else {
         console.log("it is not int 1-3");
     }
@@ -57,24 +69,65 @@ module.exports = function toReadable(number) {
             // если есть уже готовый вариант, то возвращаем его.
             decimalResult = objectDecimal[number];
         } else if (number > 20 && number < 30) {
-            decimalResult = "twenty " + objectSingle[strNumber[1]];
+            decimalResult = "twenty " + objectSingle[strNum[1]];
         } else if (number > 30 && number < 40) {
-            decimalResult = "thirty " + objectSingle[strNumber[1]];
+            decimalResult = "thirty " + objectSingle[strNum[1]];
         } else if (number > 40 && number < 50) {
-            decimalResult = "forty " + objectSingle[strNumber[1]];
+            decimalResult = "forty " + objectSingle[strNum[1]];
         } else if (number > 50 && number < 60) {
-            decimalResult = "fifty " + objectSingle[strNumber[1]];
+            decimalResult = "fifty " + objectSingle[strNum[1]];
         } else if (number > 60 && number < 70) {
-            decimalResult = "sixty " + objectSingle[strNumber[1]];
+            decimalResult = "sixty " + objectSingle[strNum[1]];
         } else if (number > 70 && number < 80) {
-            decimalResult = "seventy " + objectSingle[strNumber[1]];
+            decimalResult = "seventy " + objectSingle[strNum[1]];
         } else if (number > 80 && number < 90) {
-            decimalResult = "eighty " + objectSingle[strNumber[1]];
+            decimalResult = "eighty " + objectSingle[strNum[1]];
         } else if (number > 90 && number < 100) {
-            decimalResult = "ninety " + objectSingle[strNumber[1]];
+            decimalResult = "ninety " + objectSingle[strNum[1]];
         }
 
+        console.log("------------");
+        console.log(decimalResult);
+
         return decimalResult;
+    }
+
+    function returnHundredthsNumber(strNum) {
+        let hundredthsResult;
+
+        hundredthsResult = objectSingle[strNumber[0]] + " hundred";
+
+        if (objectHundreds[number]) {
+            hundredthsResult = objectHundreds[number];
+        } else if (strNumber[1] === "0") {
+            hundredthsResult = hundredthsResult + " " + objectSingle[strNum[2]];
+        } else if (strNumber[1] === "1") {
+            hundredthsResult =
+                hundredthsResult +
+                " " +
+                objectDecimal[Number(strNum.slice(1, 3))];
+        } else if (strNumber[1] === "2") {
+            decimalResult = "thirty " + objectSingle[Number(strNum[1])];
+        } else if (strNumber[1] === "3") {
+            decimalResult = "forty " + objectSingle[Number(strNum[1])];
+        } else if (strNumber[1] === "4") {
+            decimalResult = "fifty " + objectSingle[Number(strNum[1])];
+        } else if (strNumber[1] === "5") {
+            decimalResult = "sixty " + objectSingle[Number(strNum[1])];
+        } else if (strNumber[1] === "6") {
+            decimalResult = "seventy " + objectSingle[Number(strNum[1])];
+        } else if (strNumber[1] === "7") {
+            hundredthsResult =
+                hundredthsResult +
+                " " +
+                returnDecimalNumber(strNum.slice(1, 3));
+        } else if (strNumber[1] === "8") {
+            decimalResult = "ninety " + objectSingle[Number(strNum[1])];
+        } else if (strNumber[1] === "9") {
+            decimalResult = "ninety " + objectSingle[Number(strNum[1])];
+        }
+
+        return hundredthsResult;
     }
 
     return result;
